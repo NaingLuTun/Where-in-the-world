a1220fc80669713208e052da49bbad5236b919a0
Naing Lu Tun <nainglu7924@gmail.com>
2024-06-24 22:44:35 +0630

    fixed inital commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Where in the world/",
+  base: "/Where-in-the-world/",
   plugins: [react()],
 })
